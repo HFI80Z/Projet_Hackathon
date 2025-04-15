573da279f299fb88b330f4d34bf48f0065d68dfa
Quentin Leboucher <qleboucher1@gmail.com>
2025-04-15 16:23:39 +0200

    routes messages

--- public/index.php
+++ public/index.php
@@ -3,10 +3,10 @@ session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use App\Controllers\AuthController;
 use App\Controllers\UserController;
 use App\Controllers\AnnonceController;
+use App\Controllers\MessageController; // Ajout du nouveau contrôleur
 use App\Database\Database;
 use App\Models\AnnonceModel;
 
@@ -20,27 +20,26 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 // Routage minimaliste
 switch ($uri) {
     case '/':
-    case '/accueil': 
+    case '/accueil':
         $controller = new AnnonceController();
         $controller->index();
         break;
 
-    case '/connexion': 
+    case '/connexion':
         $controller = new AuthController();
         $controller->connexion();
         break;
 
-    case '/inscription': 
+    case '/inscription':
         $controller = new AuthController();
         $controller->inscription();
         break;
 
-    
-    case '/profil': 
+    case '/profil':
         $controller = new UserController();
         $controller->afficherProfil();
         break;
-        
+
     case '/modifier-compte':
         if (isset($_SESSION['user_id'])) {
             $controller = new UserController();
@@ -55,12 +54,46 @@ switch ($uri) {
         $controller->deconnexion();
         break;
 
-        default:
+    // Nouvelles routes pour le système de messagerie
+    case '/messages':
+        if (isset($_SESSION['user_id'])) {
+            $controller = new MessageController();
+            $controller->inbox();
+        } else {
+            header('Location: /connexion');
+        }
+        break;
+
+    case '/messages/conversation':
+        if (isset($_SESSION['user_id'])) {
+            $controller = new MessageController();
+            $controller->conversation();
+        } else {
+            header('Location: /connexion');
+        }
+        break;
+
+    case '/messages/envoyer':
+        if (isset($_SESSION['user_id']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller = new MessageController();
+            $controller->send();
+        } else {
+            header('Location: /connexion');
+        }
+        break;
+
+    case '/messages/nouveau':
+        if (isset($_SESSION['user_id'])) {
+            $controller = new MessageController();
+            $controller->newMessage();
+        } else {
+            header('Location: /connexion');
+        }
+        break;
+
+    default:
         // 404 - Page introuvable
         http_response_code(404);
         echo "Oups ! Page introuvable.";
         break;
-
-
-
-    }
+}
