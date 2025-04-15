ab67d1983a3b1838fb470f3669b568bdf24d0935
Quentin Leboucher <qleboucher1@gmail.com>
2025-04-15 14:27:08 +0200

    navbar

---
+++ templates/partials/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+   <ul>
+      <li><a href="/">Accueil</a></li>
+      <li><a href="/reservation">Réservation</a></li>
+      <li><a href="/creer-annonce">Créer une annonce</a></li>
+      <li><a href="/contact">Nous contacter</a></li>
+      <?php if (isset($_SESSION['user_id'])): ?>
+         <li><a href="/deconnexion">Déconnexion</a></li>
+      <?php else: ?>
+         <li><a href="/connexion">Connexion</a></li>
+         <li><a href="/inscription">Inscription</a></li>
+      <?php endif; ?>
+   </ul>
+</nav>
